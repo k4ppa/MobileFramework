3da4a9388d9674bbec37d19d575e909de62bfcd4
unknown <Admin@L-IT-04122.altranit.corp.altran.com>
2015-03-23 17:28:53 +0100

    Added method for close the connection with the server

--- src/mobile_framework/mobile_device.py
+++ src/mobile_framework/mobile_device.py
@@ -1,10 +1,21 @@
+
+import stormtest.ClientAPI as StormTest
+
 class MobileDevice(object):
     def __init__(self):
+        self.server = ""
+        self.description = ""
         pass
 
     
-    def openConnection(self):
-        return True
+    def openConnection(self, server, description=''):
+        return StormTest.ConnectToServer(server, description)
+
+    
+    def closeConnection(self):
+        return StormTest.ReleaseServerConnection()
+    
+    
     
     
     
