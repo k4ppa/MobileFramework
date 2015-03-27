e43d44e3007cba47a16ddb4c16f78c5689c68555
unknown <Admin@L-IT-04122.altranit.corp.altran.com>
2015-03-27 10:01:00 +0100

    Refactoring

---
+++ src/mobile_framework/connectFunctions.py
@@ -0,0 +1,67 @@
+
+import stormtest.ClientAPI as StormTest
+import stormtest.WarningCenter as WarningCenter
+
+
+def _getTestRunConfiguration():
+    params = WarningCenter.GetTestRun()
+    if params == None:      #debug mode - use a hard coded set of parameters
+        from mobile_framework.tests.test_environment import TestEnvironment
+        params = TestEnvironment.params
+        
+    return params    
+
+
+def _setUpEnvironment():
+    if not StormTest.IsUnderDaemon():
+        return _setUpTestEnvironment()
+    else:
+        return _setUpRealEnvironment()
+        
+
+def _setUpTestEnvironment():
+    from mobile_framework.tests.test_environment import TestEnvironment
+    server = TestEnvironment.getServerName()
+    slot = TestEnvironment.getSlotNumber()
+    return server, slot
+    
+    
+def _setUpRealEnvironment(self):
+    slotAllocated = StormTest.GetPhysicalAllocations()
+    server = slotAllocated[0].split(':')[0]
+    slot = slotAllocated[1][0]
+    return server, slot
+
+
+def _openConnection(server, description):
+    try:
+        StormTest.ConnectToServer(server, description)
+    except SystemExit:
+        StormTest.EndLogRegion('Open Connection', StormTest.LogRegionStyle.Fail, comment='Failed to connect to server (%s)' % server)
+        return False
+    return True
+    pass
+
+
+def _reserveSlot(slot, signalDb='', serialParams=[], videoFlag=True):
+    isReserved = StormTest.ReserveSlot(slot, signalDb, serialParams, videoFlag)
+    if isReserved is False:    
+        StormTest.EndLogRegion('Open Connection', StormTest.LogRegionStyle.Fail, comment='Failed to reserve slot (%d)' % slot)
+        
+    return isReserved 
+    
+
+def _OCRCheckRemainingChars():
+    remainingChars = StormTest.OCRGetRemainingChars()
+    print "Remaining OCR chars: " + str(remainingChars)
+    
+    if remainingChars is 0:    
+        StormTest.EndLogRegion('Open Connection', StormTest.LogRegionStyle.Fail, comment='OCR licenses has ran out. Not possible to run tests.')
+        return False
+    return True
+
+
+def _connectionEstablished(isConnectionOk):
+    if isConnectionOk:
+        return True
+    return False
--- src/mobile_framework/mobile_device.py
+++ src/mobile_framework/mobile_device.py
@@ -1,87 +1,37 @@
 
 import stormtest.ClientAPI as StormTest
-import stormtest.WarningCenter as WarningCenter 
+from mobile_framework.connectFunctions import _getTestRunConfiguration
+from mobile_framework.connectFunctions import _setUpEnvironment
+from mobile_framework.connectFunctions import _openConnection
+from mobile_framework.connectFunctions import _reserveSlot
+from mobile_framework.connectFunctions import _OCRCheckRemainingChars
+from mobile_framework.connectFunctions import _connectionEstablished
 
 
 class MobileDevice(object):
     def __init__(self):
-        self.__server = ""
-        self.__description = ""
-        self.__slot = 0
+        self._server = ""
+        self._description = ""
+        self._slot = 0
         pass
 
     
     def connect(self, description=''):
         StormTest.BeginLogRegion('Open Connection')
         
-        params = self.__getTestRunConfiguration()
+        params = _getTestRunConfiguration()
         serviceInfo = params['service']
         
-        self.__setUpEnvironment()
-        isConnected = self.__openConnection(self.__server, description)
-        isReserved = self.__reserveSlot(self.__slot, signalDb='', serialParams=[], videoFlag=True)
-        isOCRLicenseOver = self.__OCRCheckRemainingChars()
+        self._server, self._slot = _setUpEnvironment()
+        isServerConnected = _openConnection(self._server, description)
+        isSlotReserved = _reserveSlot(self._slot, signalDb='', serialParams=[], videoFlag=True)
+        
+        isConnectionOk = False
+        if isServerConnected and isSlotReserved:
+            isConnectionOk = _OCRCheckRemainingChars()
+            
+        return _connectionEstablished(isConnectionOk) 
          
-        return self.__connectionEstablished(isConnected, isReserved, isOCRLicenseOver)
-        
-        
-    def __getTestRunConfiguration(self):
-        params = WarningCenter.GetTestRun()
-        if params == None:      #debug mode - use a hard coded set of parameters
-            from mobile_framework.tests.test_environment import TestEnvironment
-            params = TestEnvironment.params
-            
-        return params    
-
-    def __setUpEnvironment(self):
-        if not StormTest.IsUnderDaemon():
-            self.__setUpTestEnvironment()
-        else:
-            self.__setUpRealEnvironment()
-            
-    
-    def __setUpTestEnvironment(self):
-        from mobile_framework.tests.test_environment import TestEnvironment
-        self.__server = TestEnvironment.getServerName()
-        self.__slot = TestEnvironment.getSlotNumber()
-        
-        
-    def __setUpRealEnvironment(self):
-        slotAllocated = StormTest.GetPhysicalAllocations()
-        self.__server = slotAllocated[0].split(':')[0]
-        self.__slot = slotAllocated[1][0]
-    
-    
-    def __openConnection(self, server, description):
-        try:
-            StormTest.ConnectToServer(server, description)
-        except SystemExit:
-            StormTest.EndLogRegion('Open Connection', StormTest.LogRegionStyle.Fail, comment='Failed to connect to server (%s)' % server)
-            return False
-        return True
-        pass
-    
-    
-    def __reserveSlot(self, slot, signalDb='', serialParams=[], videoFlag=True):
-        isReserved = StormTest.ReserveSlot(slot, signalDb, serialParams, videoFlag)
-        if isReserved is False:    
-            StormTest.EndLogRegion('Open Connection', StormTest.LogRegionStyle.Fail, comment='Failed to reserve slot (%d)' % slot)
-            
-        return isReserved 
-        
-    
-    def __OCRCheckRemainingChars(self):
-        remainingChars = StormTest.OCRGetRemainingChars()
-        if remainingChars is 0:    
-            StormTest.EndLogRegion('Open Connection', StormTest.LogRegionStyle.Fail, comment='OCR licenses has ran out. Not possible to run tests.')
-    
-        return remainingChars
-    
-    
-    def __connectionEstablished(self, isConnected, isReserved, isOCRLicenseOver):
-        if isConnected and isReserved and isOCRLicenseOver:
-            return 1
-        return isReserved
     
     
     def disconnect(self):
@@ -89,7 +39,7 @@ class MobileDevice(object):
     
     
     
-    
+
     
 
 
