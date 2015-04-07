225a20a642b1d7cdcee7243396c667186af7f18d
k4ppa <cappa.mattia@gmail.com>
2015-04-07 15:35:21 +0200

    Test tap with coordinates

--- src/mobile_framework/tests/test_android_user_actions.py
+++ src/mobile_framework/tests/test_android_user_actions.py
@@ -20,7 +20,7 @@ class Test(unittest.TestCase):
 
 
     def test_start_application_by_name_should_be_successfull(self):
-        print "TEST_START_APPLICATION_BY_NAME_SHOULD_BE_SUCCESSFULL"
+        print "TEST START APPLICATION BY NAME SHOULD BE SUCCESSFULL"
         isStarted = self.device.start("it.sky.river")
         
         self.assertEqual(isStarted, True, "App not started")
@@ -38,11 +38,20 @@ class Test(unittest.TestCase):
    
    
     def test_stop_application(self):
-        print "TEST_STOP_APPLICATION"
+        print "TEST STOP APPLICATION"
         self.device.start("it.sky.river")
         isStopped = self.device.stop()
        
         self.assertEqual(isStopped, True, "App not stopped")
+        pass
+    
+    
+    def test_tap_with_coordinates(self):
+        print "TEST TAP WITH COORDINATES"
+        self.device.start("it.sky.river")
+        isPressed = self.device.tap(30,50,0)
+        
+        self.assertEqual(isPressed, True, "Tap not happened")
         pass
    
 
