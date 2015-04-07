09594181bd3a0c8271e0fbf94a18e461ee2d603e
k4ppa <cappa.mattia@gmail.com>
2015-04-07 15:13:40 +0200

    Android device subclass

---
+++ src/mobile_framework/android_device.py
@@ -0,0 +1,21 @@
+
+import stormtest.ClientAPI as StormTest
+
+from mobile_framework.mobile_device import MobileDevice
+
+
+class AndroidDevice(MobileDevice):
+    
+    def __init__(self):
+        super(AndroidDevice, self).__init__()
+    
+    
+    def start(self, appName=''):
+        return StormTest.PressButton("START-ANDROID:" + appName)
+
+    
+    def stop(self):
+        return StormTest.PressButton("STOP-ANDROID")
+    
+    
+    
--- src/mobile_framework/mobile_device.py
+++ src/mobile_framework/mobile_device.py
@@ -38,6 +38,7 @@ class MobileDevice(object):
     
     
     
+    
 
     
 
